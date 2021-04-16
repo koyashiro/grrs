16848804148f807508c97a1e3b7f3c66287126c4
koyashiro <develop@koyashiro.net>
2021-04-16 23:58:22 +0900

    refactor: make the code testable

--- src/main.rs
+++ src/main.rs
@@ -12,15 +12,22 @@ fn main() -> Result<()> {
     let args = Cli::from_args();
     let content =
         std::fs::read_to_string(&args.path).with_context(|| format!("could not read file"))?;
-    find_matches(&content, &args.pattern);
+    find_matches(&content, &args.pattern, std::io::stdout());
 
     Ok(())
 }
 
-fn find_matches(content: &str, pattern: &str) {
+fn find_matches(content: &str, pattern: &str, mut writer: impl std::io::Write) {
     for line in content.lines() {
         if line.contains(pattern) {
-            println!("{}", line);
+            writeln!(writer, "{}", line);
         }
     }
+}
+
+#[test]
+fn find_a_match() {
+    let mut result = Vec::new();
+    find_matches("lorem ipsum\ndolar sit  amet", "lorem", &mut result);
+    assert_eq!(result, b"lorem ipsum\n");
 }
