e022454428edef74342b1a72caefe0c0e6641bc4
koyashiro <develop@koyashiro.net>
2021-04-17 00:18:19 +0900

    fix: resolve the warning

--- src/main.rs
+++ src/main.rs
@@ -12,22 +12,26 @@ fn main() -> Result<()> {
     let args = Cli::from_args();
     let content =
         std::fs::read_to_string(&args.path).with_context(|| format!("could not read file"))?;
-    find_matches(&content, &args.pattern, std::io::stdout());
+
+    find_matches(&content, &args.pattern, std::io::stdout())?;
 
     Ok(())
 }
 
-fn find_matches(content: &str, pattern: &str, mut writer: impl std::io::Write) {
+fn find_matches(content: &str, pattern: &str, mut writer: impl std::io::Write) -> Result<()> {
     for line in content.lines() {
         if line.contains(pattern) {
-            writeln!(writer, "{}", line);
+            writeln!(writer, "{}", line).with_context(|| format!("could not write"))?;
         }
     }
+
+    Ok(())
 }
 
 #[test]
 fn find_a_match() {
-    let mut result = Vec::new();
-    find_matches("lorem ipsum\ndolar sit  amet", "lorem", &mut result);
-    assert_eq!(result, b"lorem ipsum\n");
+    let mut writer = Vec::new();
+    let result = find_matches("lorem ipsum\ndolar sit  amet", "lorem", &mut writer);
+    assert!(result.is_ok());
+    assert_eq!(writer, b"lorem ipsum\n");
 }
